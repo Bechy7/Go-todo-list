efed47f31bc0e7aa71865baebadd01fea0dea8f3
Bechy7 <soeren-bechy7@hotmail.com>
2025-11-17 15:45:52 +0100

    moved commands to new file and added more commands

---
+++ commands.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+)
+
+func addTask(args []string) {
+	if len(args) == 0 {
+		fmt.Println("Please provide a task title.")
+		return
+	}
+
+	title := ""
+	for _, a := range args {
+		title += a + " "
+	}
+
+	tasks = append(tasks, Task{Title: title, Done: false})
+	fmt.Println("Added task:", title)
+}
+
+func editTask(args []string) {
+	if len(args) < 2 {
+		fmt.Println("Provide the task number and a new title.")
+		return
+	}
+
+	i := parseIndex(args[0])
+	if i == -1 || args[1] == "" {
+		return
+	}
+
+	tasks[i].Title = args[1]
+	fmt.Println("Edited task:", tasks[i].Title)
+}
+
+func listTasks() {
+	if len(tasks) == 0 {
+		fmt.Println("No tasks yet.")
+		return
+	}
+
+	for i, t := range tasks {
+		status := " "
+		if t.Done {
+			status = "✓"
+		}
+		fmt.Printf("%d. [%s] %s\n", i, status, t.Title)
+	}
+}
+
+func markDone(args []string) {
+	if len(args) == 0 {
+		fmt.Println("Provide the task number.")
+		return
+	}
+
+	i := parseIndex(args[0])
+	if i == -1 {
+		return
+	}
+
+	tasks[i].Done = true
+	fmt.Println("Marked as done:", tasks[i].Title)
+}
+
+func markUndone(args []string) {
+	if len(args) == 0 {
+		fmt.Println("Provide the task number.")
+		return
+	}
+
+	i := parseIndex(args[0])
+	if i == -1 {
+		return
+	}
+
+	tasks[i].Done = false
+	fmt.Println("Marked as undone:", tasks[i].Title)
+}
+
+func deleteTask(args []string) {
+	if len(args) == 0 {
+		fmt.Println("Provide the task number.")
+		return
+	}
+
+	i := parseIndex(args[0])
+	if i == -1 {
+		return
+	}
+
+	fmt.Println("Deleted:", tasks[i].Title)
+	tasks = append(tasks[:i], tasks[i+1:]...)
+}
+
+func parseIndex(s string) int {
+	var idx int
+	_, err := fmt.Sscanf(s, "%d", &idx)
+	if err != nil || idx < 0 || idx >= len(tasks) {
+		fmt.Println("Invalid task number.")
+		return -1
+	}
+	return idx
+}
--- main.go
+++ main.go
@@ -26,10 +26,14 @@ func main() {
 	switch command {
 	case "add":
 		addTask(os.Args[2:])
+	case "edit":
+		editTask(os.Args[2:])
 	case "list":
 		listTasks()
 	case "done":
 		markDone(os.Args[2:])
+	case "undone":
+		markUndone(os.Args[2:])
 	case "delete":
 		deleteTask(os.Args[2:])
 	default:
@@ -50,74 +54,4 @@ func loadTasks() {
 func saveTasks() {
 	data, _ := json.MarshalIndent(tasks, "", "  ")
 	os.WriteFile("tasks.json", data, 0644)
-}
-
-func addTask(args []string) {
-	if len(args) == 0 {
-		fmt.Println("Please provide a task title.")
-		return
-	}
-
-	title := ""
-	for _, a := range args {
-		title += a + " "
-	}
-
-	tasks = append(tasks, Task{Title: title, Done: false})
-	fmt.Println("Added task:", title)
-}
-
-func listTasks() {
-	if len(tasks) == 0 {
-		fmt.Println("No tasks yet.")
-		return
-	}
-
-	for i, t := range tasks {
-		status := " "
-		if t.Done {
-			status = "✓"
-		}
-		fmt.Printf("%d. [%s] %s\n", i, status, t.Title)
-	}
-}
-
-func markDone(args []string) {
-	if len(args) == 0 {
-		fmt.Println("Provide the task number.")
-		return
-	}
-
-	i := parseIndex(args[0])
-	if i == -1 {
-		return
-	}
-
-	tasks[i].Done = true
-	fmt.Println("Marked as done:", tasks[i].Title)
-}
-
-func deleteTask(args []string) {
-	if len(args) == 0 {
-		fmt.Println("Provide the task number.")
-		return
-	}
-
-	i := parseIndex(args[0])
-	if i == -1 {
-		return
-	}
-
-	fmt.Println("Deleted:", tasks[i].Title)
-	tasks = append(tasks[:i], tasks[i+1:]...)
-}
-
-func parseIndex(s string) int {
-	var idx int
-	_, err := fmt.Sscanf(s, "%d", &idx)
-	if err != nil || idx < 0 || idx >= len(tasks) {
-		fmt.Println("Invalid task number.")
-		return -1
-	}
-	return idx
 }
